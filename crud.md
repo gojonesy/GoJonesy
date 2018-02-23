15a4257603891ad405e0b3e360d70d65f7311ccb
Justin Jones <justinjones00@gmail.com>
2018-02-23 15:51:49 -0600

    Started on Post model.

---
+++ crud.md
@@ -0,0 +1,4 @@
+CREATE -- POST -- New Model instance
+RETRIEVE -- GET -- List/Search
+UPDATE -- PUT -- Edit Model Instance
+DELETE -- DELETE -- Remove Model Instance
--- gojonesy/blog/admin.py
+++ gojonesy/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp', 'lastUpdate')
+    list_filter = ['lastUpdate', 'timestamp']
+    search_fields = ['title', 'content']
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostAdmin)
